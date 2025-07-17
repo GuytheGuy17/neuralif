b39c8a7256df1116152d40ff83792f313246012e
Guy McClennan <guymcclennan1@gmail.com>
2025-07-17 16:44:04 +0100

    fixed data file

--- apps/data.py
+++ apps/data.py
@@ -1,92 +1,53 @@
-from glob import glob
-
-import numpy as np
+import os
+import glob
 import torch
-from scipy.sparse import coo_matrix
 from torch_geometric.data import Data
-
 from torch_geometric.loader import DataLoader
-
+from torch_geometric.utils import from_scipy_sparse_matrix
+from scipy.sparse import coo_matrix
 
 def matrix_to_graph_sparse(A, b):
-    edge_index = torch.tensor(list(map(lambda x: [x[0], x[1]], zip(A.row, A.col))), dtype=torch.long)
-    edge_features = torch.tensor(list(map(lambda x: [x], A.data)), dtype=torch.float)
-    node_features = torch.tensor(list(map(lambda x: [x], b)), dtype=torch.float)
-
-    # diag_elements = edge_index[:, 0] == edge_index[:, 1]
-    # node_features = edge_features[diag_elements]
-    # node_features = torch.cat((node_features, torch.tensor(list(map(lambda x: [x], b)), dtype=torch.float)), dim=1)
-    
-    # Embed the information into data object
-    data = Data(x=node_features, edge_index=edge_index.t().contiguous(), edge_attr=edge_features)
-    return data
-
+    """Internal helper to convert a SciPy COO matrix to a PyG Data object."""
+    edge_index = torch.tensor(np.array([A.row, A.col]), dtype=torch.long)
+    edge_attr = torch.tensor(A.data, dtype=torch.float).unsqueeze(1)
+    node_features = torch.tensor(b, dtype=torch.float).unsqueeze(1)
+    return Data(x=node_features, edge_index=edge_index, edge_attr=edge_attr)
 
 def matrix_to_graph(A, b):
+    """Converts a SciPy sparse matrix to a PyG Data object."""
     return matrix_to_graph_sparse(coo_matrix(A), b)
 
-
-def graph_to_matrix(data, normalize=False):
-    A = torch.sparse_coo_tensor(data.edge_index, data.edge_attr[:, 0].squeeze(), requires_grad=False)
-    b = data.x[:, 0].squeeze()
-    
-    if normalize:
-        b = b / torch.linalg.norm(b)
-    
+def graph_to_matrix(data):
+    """Converts a PyG Data object back to a sparse torch tensor."""
+    A = torch.sparse_coo_tensor(data.edge_index, data.edge_attr.squeeze(), requires_grad=False)
+    b = data.x.squeeze()
     return A, b
 
-
-def get_dataloader(dataset, n=0, batch_size=1, spd=True, mode="train", size=None, graph=True):
-    # Setup datasets
-    
-    if dataset == "random":
-        data = FolderDataset(f"./data/Random/{mode}/", n, size=size, graph=graph)
-    
-    else:
-        raise NotImplementedError("Dataset not implemented, Available: random")
-    
-    # Data Loaders
-    if mode == "train":
-        dataloader = DataLoader(data, batch_size=batch_size, shuffle=True)
-    else:
-        dataloader = DataLoader(data, batch_size=1, shuffle=False)
-    
-    return dataloader
-
-
 class FolderDataset(torch.utils.data.Dataset):
-    def __init__(self, folder, n, graph=True, size=None) -> None:
+    """A generic dataset that loads all '.pt' graph files from a specified folder."""
+    def __init__(self, folder_path):
         super().__init__()
-        
-        self.graph = True
-        assert self.graph, "Graph keyword is depracated, only graph=True is supported."
-                
-        if n != 0:
-            if self.graph:
-                self.files = list(filter(lambda x: x.split("/")[-1].split('_')[0] == str(n), glob(folder+'*.pt')))
-            else:
-                self.files = list(filter(lambda x: x.split("/")[-1].split('_')[0] == str(n), glob(folder+'*.npz')))
-        else:
-            file_ending = "pt" if self.graph else "npz"
-            self.files = list(glob(folder+f'*.{file_ending}'))
-        
-        if size is not None:
-            assert len(self.files) >= size, f"Only {len(self.files)} files found in {folder} with n={n}"
-            self.files = self.files[:size]
-        
-        if len(self.files) == 0:
-            raise FileNotFoundError(f"No files found in {folder} with n={n}")
-        
+        self.files = sorted(glob.glob(os.path.join(folder_path, '*.pt')))
+        if not self.files:
+            raise FileNotFoundError(f"CRITICAL: No '.pt' files were found in the directory: {folder_path}")
+
     def __len__(self):
         return len(self.files)
-    
+
     def __getitem__(self, idx):
-        if self.graph:
-            g = torch.load(self.files[idx], weights_only=False)
-        
-        else:
-            # deprecated...
-            d = np.load(self.files[idx], allow_pickle=True)
-            g = matrix_to_graph(d["A"], d["b"])
-        
-        return g
+        return torch.load(self.files[idx], weights_only=False)
+
+def get_dataloader(dataset_path, batch_size=1, mode="train", **kwargs):
+    """
+    Creates a DataLoader by loading graph files from the provided directory path.
+    e.g., dataset_path/train or dataset_path/val
+    """
+    folder_path = os.path.join(dataset_path, mode)
+    dataset = FolderDataset(folder_path=folder_path)
+    
+    # This function now ignores unused parameters like 'n' and 'spd' for simplicity.
+    
+    print(f"Successfully created a '{mode}' dataloader.")
+    print(f" -> Loading {len(dataset)} samples from: {os.path.abspath(folder_path)}")
+
+    return DataLoader(dataset, batch_size=batch_size, shuffle=(mode == "train"))
